0f9559a0e254ac04da1572e9b73fdde60cab30b1
Ylidox <alievartiom2017@gmail.com>
2022-10-15 01:43:37 +0300

    add css for main container

--- frontend-plan-events/componets/MainContainer.js
+++ frontend-plan-events/componets/MainContainer.js
@@ -1,29 +1,58 @@
 import Head from "next/head"
-
+import Link from "next/link"
+import styles from "../styles/MainContainer.module.css"
 
 function MainContainer({ children, keywords }) {
     return (
         <>
             <Head>
-                <meta keywords={"hh, jjjjjj" + keywords}></meta>
+                <meta keywords={keywords}></meta>
                 <title>Главная страница</title>
             </Head>
-            <div className="navbar">
+            {/* <div className="navbar">
                 <a href={'/'}>Главная</a>
                 <a href={'/users'}>Пользователи</a>
+            </div> */}
+            <header className={styles.header}>
+                <div className={styles.header_block}>
+                    <div>LOGO</div>
+                    <h1>Plan Events</h1>
+                </div>
+            </header>
+            
+            <div className={styles.menu}>
+                <ul>
+                    <Link href={'/'}>
+                    <li>
+                        <div className="icon">IC</div>
+                        <p>События</p>
+                    </li>
+                    </Link>
+                    <li>
+                        <div className="icon">IC</div>
+                        <p>Настройки группы</p>
+                    </li>
+                    <li>
+                        <div className="icon">IC</div>
+                        <p>Чат</p>
+                    </li>
+                    <li>
+                        <div className="icon">IC</div>
+                        <p>Список покупок</p>
+                    </li>
+                    <li>
+                        <div className="icon">IC</div>
+                        <p>Затраты</p>
+                    </li>
+                </ul>
             </div>
-            <div>
+            <div className={styles.content}>
                 {children}
             </div>
-            <style jsx>
-                {`
-                  .navbar {
-                      background: orange;
-                      padding: 15px;
-                  }
-                 
-              `}
-            </style>
+            
+            <footer className={styles.footer}>
+                Lorem Lorem Lorem
+            </footer>
         </>
     );
 }
--- frontend-plan-events/pages/index.js
+++ frontend-plan-events/pages/index.js
@@ -4,19 +4,35 @@ import styles from "../styles/Home.module.css";
 import MainContainer from "../componets/MainContainer";
 
 let array = [
-  { name: "event_name_1", id: 1 },
-  { name: "event_name_2", id: 2 },
+  { name: "event_name_1", id: 1, description: "Lorem ipsum dolor sit amet", data:"25.10.2022" },
+  { name: "event_name_2", id: 2, description: "Lorem ipsum dolor sit amet", data:"03.11.2022" },
+  { name: "event_name_3", id: 3, description: "Lorem ipsum dolor sit amet", data:"12.11.2022" },
+  { name: "event_name_1", id: 1, description: "Lorem ipsum dolor sit amet", data:"25.10.2022" },
+  { name: "event_name_2", id: 2, description: "Lorem ipsum dolor sit amet", data:"03.11.2022" },
+  { name: "event_name_3", id: 3, description: "Lorem ipsum dolor sit amet", data:"12.11.2022" },
+  { name: "event_name_1", id: 1, description: "Lorem ipsum dolor sit amet", data:"25.10.2022" },
+  { name: "event_name_2", id: 2, description: "Lorem ipsum dolor sit amet", data:"03.11.2022" },
+  { name: "event_name_3", id: 3, description: "Lorem ipsum dolor sit amet", data:"12.11.2022" },
+  { name: "event_name_1", id: 1, description: "Lorem ipsum dolor sit amet", data:"25.10.2022" },
+  { name: "event_name_2", id: 2, description: "Lorem ipsum dolor sit amet", data:"03.11.2022" },
+  { name: "event_name_3", id: 3, description: "Lorem ipsum dolor sit amet", data:"12.11.2022" },
 ];
 
 export default function Home() {
   return (
     <MainContainer keywords={"home"}>
-      <div>
-        {array.map((v) => {
+      <div className={styles.events}>
+        {array.map((v, index) => {
           return (
-            <Link href={"/events/" + v.id + "/basket"}>
-              <a>
-              {v.id} {v.name}
+            <Link href={"/events/" + v.id + "/basket"} key={index}>
+              <a className={styles.event}>
+                <div className={styles.event_head}>
+                  <h2>{v.name}</h2>
+                  <p>{v.data} </p>
+                </div>
+                <div className={styles.event_description}>
+                  <p>{v.description} </p>
+                </div>
               </a>
             </Link>
           );
