d2ea0bc30b88631e19e37d9d6c3b24fa71a84453
ilau020203 <antip19052016@gmail.com>
2022-11-12 12:58:54 +0300

    add test

--- frontend-plan-events/pages/SignIn.js
+++ frontend-plan-events/pages/SignIn.js
@@ -8,10 +8,6 @@ import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 
 const SigninSchema = Yup.object().shape({
-  phone: Yup.string()
-    .min(2, "Too Short!")
-    .max(50, "Too Long!")
-    .required("Required"),
   username: Yup.string()
     .min(2, "Too Short!")
     .max(50, "Too Long!")
@@ -20,21 +16,16 @@ const SigninSchema = Yup.object().shape({
     .min(4, "Too Short!")
     .max(50, "Too Long!")
     .required("Required"),
-  email: Yup.string().email("Invalid email").required("Required"),
 });
 
 const SignIn = () => {
   const auth = useContext(AuthContext);
   const message = useMessage();
   const { loading, request, error, clearError } = useHttp();
-  const [form, setForm] = useState({
-    email: "",
-    password: "",
-  });
 
   const [styleForm, setStyleForm] = useState({
     username: styles.input_field,
-    password: styles.input_field
+    password: styles.input_field,
   });
 
   useEffect(() => {
@@ -43,11 +34,7 @@ const SignIn = () => {
   }, [error, message, clearError]);
 
 
-  const changeHandler = (event) => {
-    setForm({ ...form, [event.target.name]: event.target.value });
-  };
-
-  const loginHandler = async () => {
+  const loginHandler = async (form) => {
     try {
       const data = await request("/auth/login", "POST", { ...form });
       console.log(data);
@@ -58,42 +45,37 @@ const SignIn = () => {
     }
   };
 
-  const conditionFieldRender = (errors, touched,field) => {
-		let elem = {};
-    if(errors[field] && touched[field]){
-      if(errors[field] == "Required"){
+  const conditionFieldRender = (errors, touched, field) => {
+    let elem = {};
+    if (errors[field] && touched[field]) {
+      if (errors[field] == "Required") {
         styleForm[field] = styles.error_input_field;
-				elem = (
-					<div className={styles.form_error}>{errors[field]}</div>
-				);
-      }else{
+        elem = <div name={`${field}-error-messege`} className={styles.form_error}>{errors[field]}</div>;
+      } else {
         styleForm[field] = styles.error_input_field;
-        elem =(
-          <div className={styles.form_error}>{errors[field]}</div>
-        );
+        elem = <div name={`${field}-error-messege`}  className={styles.form_error}>{errors[field]}</div>;
       }
-    }else{
+    } else {
       styleForm[field] = styles.input_field;
-			elem = (null);
-    }
-		return elem;
-  }
+      elem = null;
+    } 
+    return elem;
+  };
 
   return (
     <div className={styles.root}>
       <div className={styles.content}>
-        
         <div className="card blue darken-1">
           <div className="card-content white-text">
             <div className={styles.logo}>
-              <img src="/icons/logo.png"/>
+              <img src="/icons/logo.png" />
             </div>
 
             <div className={styles.title}>
               <h3>Добрый день!</h3>
               <p>Введите свой логин и пароль</p>
             </div>
-            
+
             <Formik
               initialValues={{
                 email: "",
@@ -104,58 +86,56 @@ const SignIn = () => {
               validationSchema={SigninSchema}
               onSubmit={loginHandler}
             >
-              {({ errors, touched }) =>{ return (
-                <Form>
-                  <div  className={styles.form}>
-                  {conditionFieldRender(errors, touched,"username")}
-                    <div className={styleForm.username}>
-                      <div className={styles.img_email} >
-                        <img src="/icons/email.png"/>
-                      </div>
-                      
-                      <Field
-                        placeholder="Введите логин"
-                        id="login"
-                        type="text"
-                        name="username"
-                        className="yellow-input"
-                      />
-                      
-                    </div>
-                    
-                    {conditionFieldRender(errors, touched,"password")}
-                    <div className={styleForm.password}>
-                      <div className={styles.img_container}>
-                        <img src="/icons/password.png"/>
-                      </div>
-                      
-                      <Field
-                        placeholder="Введите пароль"
-                        id="password"
-                        type="password"
-                        name="password"
-                        className="yellow-input"
-                      />
-                      
-                    </div>
-                  </div>
-                  
-                  <div className={styles.card_action}>
-                    <Link href={"/SignUp"}>Нет аккаунта? Регистрация</Link>
-                    <button
-                      className={styles.button_signup}
-                      style={{ marginRight: 10 }}
-                      disabled={loading}
-                      // onClick={loginHandler}
-                      type="submit"
-                    >
-                      Войти
-                    </button>
-                  </div>
-                </Form>
-              )}}
-            </Formik>
+              {({ errors, touched }) => {
+                return (
+                  <Form>
+                    <div className={styles.form}>
+                      {conditionFieldRender(errors, touched, "username")}
+                      <div className={styleForm.username}>
+                        <div className={styles.img_email}>
+                          <img src="/icons/email.png" />
+                        </div>
 
+                        <Field
+                          placeholder="Введите логин"
+                          id="login"
+                          type="text"
+                          name="username"
+                          className="yellow-input"
+                        />
+                      </div>
+
+                      {conditionFieldRender(errors, touched, "password")}
+                      <div className={styleForm.password}>
+                        <div className={styles.img_container}>
+                          <img src="/icons/password.png" />
+                        </div>
+
+                        <Field
+                          placeholder="Введите пароль"
+                          id="password"
+                          type="password"
+                          name="password"
+                          className="yellow-input"
+                        />
+                      </div>
+                    </div>
+
+                    <div className={styles.card_action}>
+                      <Link href={"/SignUp"}>Нет аккаунта? Регистрация</Link>
+                      <button
+                        className={styles.button_signup}
+                        style={{ marginRight: 10 }}
+                        disabled={loading}
+                        type="submit"
+                      >
+                        Войти
+                      </button>
+                    </div>
+                  </Form>
+                );
+              }}
+            </Formik>
           </div>
         </div>
       </div>
---
+++ frontend-plan-events/test/py-seleneum/test_registration.py
@@ -0,0 +1,143 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import random
+import time
+
+
+link = 'http://localhost:3000/SignUp'
+
+
+def test_short_username(browser):
+    browser.get(link)
+    browser.implicitly_wait(10)
+    input_username = browser.find_element(By.CSS_SELECTOR, "[placeholder='Введите логин']")
+    input_username.send_keys("a")
+
+    button_enter = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button_enter.click()
+
+    username_error_div_information = browser.find_element(By.NAME, 'username-error-messege')
+    assert username_error_div_information.text=="Too Short!"
+    password_error_div_information = browser.find_element(By.NAME, 'password-error-messege')
+    assert password_error_div_information.text=="Required" 
+    email_error_div_information = browser.find_element(By.NAME, 'email-error-messege')
+    assert email_error_div_information.text=="Required"
+    phone_error_div_information = browser.find_element(By.NAME, 'phone-error-messege')
+    assert phone_error_div_information.text=="Required" 
+    time.sleep(1)
+
+
+
+
+
+def test_long_username(browser):
+    browser.get(link)
+    browser.implicitly_wait(10)
+
+    input_username = browser.find_element(By.CSS_SELECTOR, "[placeholder='Введите логин']")
+    input_username.send_keys("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+    button_enter = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button_enter.click()
+
+    username_error_div_information = browser.find_element(By.NAME, 'username-error-messege')
+    password_error_div_information = browser.find_element(By.NAME, 'password-error-messege')
+
+    assert username_error_div_information.text=="Too Long!" 
+    assert password_error_div_information.text=="Required" 
+    email_error_div_information = browser.find_element(By.NAME, 'email-error-messege')
+    assert email_error_div_information.text=="Required"
+    phone_error_div_information = browser.find_element(By.NAME, 'phone-error-messege')
+    assert phone_error_div_information.text=="Required" 
+    time.sleep(1)
+
+def test_empty_all_field(browser):
+    browser.get(link)
+    browser.implicitly_wait(10)
+
+    button_enter = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button_enter.click()
+
+    username_error_div_information = browser.find_element(By.NAME, 'username-error-messege')
+    password_error_div_information = browser.find_element(By.NAME, 'password-error-messege')
+
+    assert username_error_div_information.text=="Required" 
+    assert password_error_div_information.text=="Required"
+    email_error_div_information = browser.find_element(By.NAME, 'email-error-messege')
+    assert email_error_div_information.text=="Required"
+    phone_error_div_information = browser.find_element(By.NAME, 'phone-error-messege')
+    assert phone_error_div_information.text=="Required"  
+    time.sleep(1)
+
+
+def test_short_password(browser):
+    browser.get(link)
+    browser.implicitly_wait(10)
+
+    input_password = browser.find_element(By.CSS_SELECTOR, "[placeholder='Введите пароль']")
+    input_password.send_keys("a")
+
+    button_enter = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button_enter.click()
+
+    username_error_div_information = browser.find_element(By.NAME, 'username-error-messege')
+    password_error_div_information = browser.find_element(By.NAME, 'password-error-messege')
+
+    assert password_error_div_information.text=="Too Short!" 
+    assert username_error_div_information.text=="Required"
+    email_error_div_information = browser.find_element(By.NAME, 'email-error-messege')
+    assert email_error_div_information.text=="Required"
+    phone_error_div_information = browser.find_element(By.NAME, 'phone-error-messege')
+    assert phone_error_div_information.text=="Required"  
+    
+    time.sleep(1)
+
+
+def test_long_password(browser):
+    browser.get(link)
+    browser.implicitly_wait(10)
+
+    input_password = browser.find_element(By.CSS_SELECTOR, "[placeholder='Введите пароль']")
+    input_password.send_keys("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa")
+
+    button_enter = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button_enter.click()
+
+    username_error_div_information = browser.find_element(By.NAME, 'username-error-messege')
+    password_error_div_information = browser.find_element(By.NAME, 'password-error-messege')
+
+    assert password_error_div_information.text=="Too Long!" 
+    assert username_error_div_information.text=="Required"
+
+    email_error_div_information = browser.find_element(By.NAME, 'email-error-messege')
+    assert email_error_div_information.text=="Required"
+    phone_error_div_information = browser.find_element(By.NAME, 'phone-error-messege')
+    assert phone_error_div_information.text=="Required"  
+    
+    time.sleep(1)
+
+def test_success_password(browser):
+    # Зайти на сайт https://mail.yandex.ru/
+    browser.get(link)
+    browser.implicitly_wait(10)
+
+    input_username = browser.find_element(By.CSS_SELECTOR, "[placeholder='Введите логин']")
+    input_username.send_keys("asdf" + str(random.randint(0, 100000000000000)))
+
+    input_password = browser.find_element(By.CSS_SELECTOR, "[placeholder='Введите пароль']")
+    input_password.send_keys("asdf")
+
+    input_phone = browser.find_element(By.CSS_SELECTOR, "[placeholder='Введите номер телефона']")
+    input_phone.send_keys(str(random.randint(10000000000, 99999999999)))
+
+    input_password = browser.find_element(By.CSS_SELECTOR, "[placeholder='Введите email']")
+    input_password.send_keys("asdf" + str(random.randint(1000, 999999)) + "p19052016@gmail.com")
+
+    button_enter = browser.find_element(By.CSS_SELECTOR, "button[type='submit']")
+    button_enter.click()
+
+    time.sleep(4)
+
+    url = browser.current_url
+    assert 'SignIn' in url
+
